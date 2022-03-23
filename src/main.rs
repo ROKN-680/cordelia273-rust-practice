5c926d07f48e524f3782ac3d20ae6554da41397a
전윤민 <cordelia273@naver.com>
2022-03-23 07:20:41 +0000

    Number Match Game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    println!("Secret Number: {}", secret_number);
+
+    loop {
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("cannot read from user");
+
+        let guess: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Less!"),
+            Ordering::Equal => {
+                println!("Yes!");
+                break;
+            },
+            Ordering::Greater => println!("Greater!"),
+        }
+    }
 }
